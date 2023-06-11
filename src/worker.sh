509f8ebce28f53691ced3f0c970589d08d8382bc
KMANDY <kmandy@KMANDYs-MacBook-Pro.local>
2023-06-11 04:24:19 +0200

    hotfix worker

--- src/worker.sh
+++ src/worker.sh
@@ -7,7 +7,7 @@ else
 echo -e "\033[0m\n\033[0m\033[31m  EXPORT TOKEN FAILURE! \033[0m"
 fi
 
-if curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.111:6443 K3S_TOKEN=${TOKEN} sh -
+if curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/token.env) sh -
 then
 echo -e "\n\033[32m The installation script with the K3S_URL and K3S_TOKEN environment variables SUCCESS \033[0m"
 else
