5e0cc8f1eda5378fadb6a24c2c46540545f7a822
DESKTOP-SJPL9IE\DVSavchenko <dvs.rocket@gmail.com>
2023-12-08 20:35:35 +0100

    update

--- bonus/scripts/update_repo.sh
+++ bonus/scripts/update_repo.sh
@@ -2,10 +2,11 @@
 
 # create user name and root for gitlab
 GITLAB_PASS=$(sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode)
-echo "machine gitlab.k3d.gitlab.com
+sudo echo "machine gitlab.k3d.gitlab.com
 login root
 password ${GITLAB_PASS}" > ~/.netrc
-chmod 600 /root/.netrc
+sudo mv ~/.netrc /root/
+sudo chmod 600 /root/.netrc
 
 # clone repo
 sudo git clone http://gitlab.k3d.gitlab.com/root/buthor.git git_repo
@@ -23,5 +24,9 @@ cd git_repo
 sudo git add *
 sudo git commit -m "update"
 sudo git push
+cd ..
 
 sudo kubectl apply -f ../confs/deploy.yaml
+
+# Warning port-forward
+echo -e "${GREEN}PORT-FORWARD : sudo kubectl port-forward svc/svc-wil -n dev 8888:8080${RESET}"
